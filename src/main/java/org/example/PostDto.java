e88a24cc76d8fba937c9d6ccc7b4478cc1f849d5
Paweł <morhenmail@gmail.com>
2023-01-15 08:59:08 +0100

    Add constructors for PostDto

--- src/main/java/org/example/PostDto.java
+++ src/main/java/org/example/PostDto.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostDto {
   long id;
   int userId;
